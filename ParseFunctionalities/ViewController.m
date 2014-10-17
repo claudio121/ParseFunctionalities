9dbdd3d28af7af8817e61f9b77346c59a96dc403
claudio121 <caromano@fiu.edu>
2014-10-17 09:32:57 -0400

    Initial Commit

---
+++ ParseFunctionalities/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ParseFunctionalities
+//
+//  Created by Claudio Romano on 10/17/14.
+//  Copyright (c) 2014 FIU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ParseFunctionalities/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParseFunctionalities
+//
+//  Created by Claudio Romano on 10/17/14.
+//  Copyright (c) 2014 FIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
